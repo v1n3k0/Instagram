b643c6cf0f00d21d1f80e896da88e7274de4f34f
v1n3k0 <v1n1c1u5_1@hotmail.com>
2017-08-21 16:47:21 -0300

    Aula 281

--- ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
@@ -10,6 +10,7 @@ package com.parse.starter.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -70,9 +71,29 @@ public class MainActivity extends AppCompatActivity {
       case R.id.action_condiguracoes:
         return true;
       case R.id.action_compartilhar:
+        compartilharFoto();
         return true;
       default:
         return super.onOptionsItemSelected(item);
+    }
+
+  }
+
+  private void compartilharFoto(){
+
+    Intent intent = new Intent( Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
+    startActivityForResult( intent, 1);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    //Testar processo de retorno de dados
+    if( requestCode == 1 && resultCode == RESULT_OK && data != null ){
+
+
+
     }
 
   }
